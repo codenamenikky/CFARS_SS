45d1698f23889eea0a5baaf08e8f4a2650e7230c
Nikhil Kondabala <codenamenikky@gmail.com>
2018-11-26 10:31:56 -0500

    Deleting Argument Testing

--- arguemnttesting.py
+++
@@ -1,9 +0,0 @@
-import argparse
-if __name__=="__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("input_filename", help="print this requires the input filename")
-    parser.add_argument("config_file", help="this reuquires the excel configuration file")
-    args = parser.parse_args()
-    print args.input_filename
-    print args.config_file
-    return args.input_filename, args.config_file
